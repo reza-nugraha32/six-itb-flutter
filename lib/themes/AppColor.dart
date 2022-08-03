1cc4febfd7c166552dc1b5186362de5b6d08a0e6
Harukii373 <12321032@mahasiswa.itb.ac.id>
2022-08-03 22:22:25 +0700

    Added assets, theme color, updated pubspec

---
+++ lib/themes/AppColor.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const PrimaryColor = const Color(0xFF003366);
+const PrimaryColorLight = const Color(0xFF9cafc2);
+const PrimaryDark = const Color(0xFF2a2a2a);
+
+const Background = const Color(0xFFfffdf7);
+const TextColor = const Color(0xFFffffff);
+const TextColorDark = const Color(0xFF2a2a2a);
+
+class MyTheme {
+  static final ThemeData defaultTheme = _buildMyTheme();
+
+  static ThemeData _buildMyTheme() {
+    final ThemeData base = ThemeData.light();
+
+    return base.copyWith(
+      accentColor: PrimaryColor,
+      accentColorBrightness: Brightness.dark,
+      primaryColor: PrimaryColor,
+      primaryColorDark: PrimaryDark,
+      primaryColorLight: PrimaryColorLight,
+      primaryColorBrightness: Brightness.dark,
+      buttonTheme: base.buttonTheme.copyWith(
+        buttonColor: PrimaryColor,
+        textTheme: ButtonTextTheme.primary,
+      ),
+      scaffoldBackgroundColor: Background,
+      cardColor: Background,
+      textSelectionColor: PrimaryColorLight,
+      backgroundColor: Background,
+    );
+  }
+}
